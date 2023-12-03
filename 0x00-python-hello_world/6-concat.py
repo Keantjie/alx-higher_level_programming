c11ad71a5aa5fb27cef164bac933476fac457c9d
Keantjie <keamom97@gmail.com>
2023-12-03 08:33:37 -0800

    commit

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 += (" " + str2)
-print(f"Welccome to {}!".format(str1))
+str1 += " " + str2
+print("Welccome to {}!".format(str1))
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,11 +1,10 @@
-#ifndef LISTS__H
+#ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
-
-/**
- * struct list_s - singly linked list
- * @n: the interger
- * @next: point next to node
+/*
+ * struct list_s - singly linked
+ * @n: integer
+ * @next: points
  */
 typedef struct list_s
 {
@@ -17,5 +16,4 @@ size_t print_listint(const list_t *h);
 list_t *add_nodeint(list_t **head, const int n);
 void free_listint(list_t *head);
 int check_cycle(list_t *list);
-
 #endif
