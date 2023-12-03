5d71066d22e4b72e2a1b4b64f32ebbec12f2383a
Keantjie <keamom97@gmail.com>
2023-12-03 12:14:43 -0800

    import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-def main():
+if __name__ == "__main__":
     a = 10
     b = 5
-    symbol = ['+', '-', '*', '/']
-    op = {0: add, 1: sub, 2: mul, 3: div}
-    for j in op:
-        print('{:d} {:} {:d} = {:d}'.format(a, symbol[j], b, op[j](a, b)))
-
-        if __name__ == "__main__":
-            main()
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
