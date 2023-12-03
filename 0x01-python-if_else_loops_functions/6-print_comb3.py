d8beab636f528b1e44a45c26f959e6bace35fd66
Keantjie <keamom97@gmail.com>
2023-12-03 10:23:34 -0800

    print

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(i + 1, 10):
+        print('{:d}{:d}'.format(a, b), '\n' if (a == 8 and b == 9) else ', ',
+              sep='', end='')
