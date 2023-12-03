64831ca05da81b0e8083a7ee521e14354ef740ab
Keantjie <keamom97@gmail.com>
2023-12-03 12:20:27 -0800

    import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,10 @@ def main():
     l = len(sys.argv)
     print('{:d} argument{:}'.format(l - 1, '.' if l == 1 else
                                     (':' if l == 2 else 's:')))
-    a = 1
+    i = 1
     for arg in sys.argv[1:]:
-        print("{:d}: {}".format(a, arg))
-        a += 1
+        print("{:d}: {}".format(i, arg))
+        i += 1
 
 if __name__ == "__main__":
     main()
