d386b095cf0fc9113b4de3b6415e3b086ab3d616
Keantjie <keamom97@gmail.com>
2023-12-04 12:33:39 -0800

    corrections

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
-    return (l, sentence[0] if l > 0 else None)
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return (None)
-    my_list.sort()
-    return (my_list[-1])
+        return None
+    m = my_list[0]
+    for i in my_list:
+        if i > m:
+            m = i
+    return m
