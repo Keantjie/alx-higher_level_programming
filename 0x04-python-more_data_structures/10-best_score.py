1a97a0c78b57f85a4b8594618d8b5dd7994ead31
Keantjie <keamom97@gmail.com>
2023-12-06 11:58:40 -0800

    correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict:
-        return sorted(my_dict)[-1]
+def best_score(a_dictionary):
+
+    max_val = 0
+    victor = None
+    if type(a_dictionary) is dict:
+        for key, value in a_dictionary.items():
+            if value > max_val:
+                max_val = value
+                victor = key
+    return victor
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,18 @@
 #!/usr/bin/python3
-def to_minustract(list_num):
-    to_minus = 0
-    maximum_list = max(list_num)
-
-    for n in list_num:
-        if maximum_list > n:
-            to_minus += n
-
-    return (maximum_list - to_minus)
-
-
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-
-    if not isinstance(roman_string, str):
-        return 0
-
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
-
-    number= 0
-    last_rom = 0
-    list_number= [0]
-
-    for ch in roman_string:
-        for r_numberin list_keys:
-            if r_number== ch:
-                if rom_n.get(ch) <= last_rom:
-                    number+= to_minustract(list_num)
-                    list_number= [rom_n.get(ch)]
-                else:
-                    list_num.append(rom_n.get(ch))
-
-                last_rom = rom_n.get(ch)
-
-    number+= to_minustract(list_num)
-
-    return (number)
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
+    roman_list = list(roman_string.upper())
+    result = 0
+    prev = 0
+    for letter in roman_list:
+        if letter in roman_dict:
+            result += roman_dict[letter]
+            if roman_dict[letter] > prev:
+                result -= prev * 2
+            prev = roman_dict[letter]
+        else:
+            return (0)
+    return (result)
