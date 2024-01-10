765f697ef23501094b8b6c0b3f13746303a86334
Keantjie <keamom97@gmail.com>
2024-01-09 18:15:36 -0800

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def inherits_from(obj, a_class):
+"""Checks if object is an instance of class, or if the object is an instance"""
+    if not isinstance(a_class, type):
+        raise TypeError("a_class type must be type")
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
