365b1ece2475073adf8feaa8a228bb2ccd26cfd2
Keantjie <keamom97@gmail.com>
2024-01-09 18:52:09 -0800

    corrections

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ has the Mylist class"""
-class Mylist(list):
-    def __init__(self):
-        super().__init__()
-        def print_sorted(self):
-            print(sorted(self))
+class MyList(list):
+    """subclass to add methods to list class"""
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
