74a9f62659d3910c15225de924d427df60c01b2d
Keantjie <keamom97@gmail.com>
2024-01-09 17:39:05 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" has the Mylist class"""
+class Mylist(list):
+    def __init__(self):
+        super().__init__()
+        def print_sorted(self):
+            print(sorted(self))
