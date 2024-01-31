409792becb06e9b1dd83e6ad2d3a1d9d65d60342
Keantjie <keamom97@gmail.com>
2024-01-31 13:09:49 -0800

    correction

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 """
-module to print names
+module for matrix maths
 """
 
 
-def say_my_name(first_name, last_name=""):
-    if isinstance(first_name, str) and isinstance(last_name, str):
-        print('My name is {:} {:}'.format(first_name, last_name))
-    else:
-        raise TypeError('{:} must be a string'
-                        .format('last_name' if isinstance(first_name, str)
-                                else 'first_name'))
+def matrix_divided(matrix, div):
+    """ divides matrix by input divisor"""
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+    new = []
+    rowlen = len(matrix[0])
+    for row in matrix:
+        sub = []
+        if len(row) != rowlen:
+            raise TypeError('Each row of the matrix must have the same size')
+        for i in range(len(row)):
+            if not isinstance(row[i], (int, float)):
+                raise TypeError('matrix must be a matrix (list of lists) of '
+                                'integers/floats')
+            sub.append(round((row[i] / div), 2))
+        new.append(sub)
+    return new
